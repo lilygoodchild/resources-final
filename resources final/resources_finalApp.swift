4b45078913b7dc82ca2dbacc2ded1e9959b1e0dd
Lily <lsmgoodchild@icloud.com>
2023-08-03 14:29:31 +0100

    Initial Commit

---
+++ resources final/resources_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  resources_finalApp.swift
+//  resources final
+//
+//  Created by scholar on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct resources_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
